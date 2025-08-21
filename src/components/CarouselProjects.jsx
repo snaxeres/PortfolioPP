b2636f4b23f75719dd6d7a41025dd0db979171d9
Enru <enriquealegreal@gmail.com>
2025-08-21 16:41:39 -0300

    modifique proyecto

--- src/components/CarouselProjects.jsx
+++ src/components/CarouselProjects.jsx
@@ -8,7 +8,6 @@ import CardProject from './CardProject'
 
 const projects = [
   { id:1, title:'E-commerce-gym', image:'https://plus.unsplash.com/premium_photo-1754344901213-57465a7e6afc?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', tools:['Html','Css','Bootstrap','Sass'], github:'https://github.com/snaxeres/EntregaFinal-commerce' },
-  { id:2, title:'E-commerce-grow', image:'https://picsum.photos/seed/2/800/500', tools:['Javascript','Librerias'], github:'https://snaxeres.github.io/EntregaFinal-G.e-commerce/' },
   { id:3, title:'E-commerce-react', image:'https://picsum.photos/seed/3/800/500', tools:['React','Javascript','Librerias',], github:'https://github.com/snaxeres/EntregaFinal-React-commerce' },
   { id:4, title:'E-commerce-Backend-I', image:'https://picsum.photos/seed/4/800/500', tools:['Bcrypt','cookie-parser','express', 'mongoose', 'jsonwebtoken', 'Librerias'], github:'https://github.com/snaxeres/EntregaFinal-Back-commerce' },
   { id:5, title:'E-commerce-Backend-II', image:'https://picsum.photos/seed/5/800/500', tools:['Bcrypt','cookie-parser','express', 'mongoose', 'socket.io', 'mongodb', 'Librerias'], github:'https://github.com/snaxeres/EntregaFinal-Back-2' },
@@ -40,44 +39,5 @@ export default function CarouselProjects(){
     </div>
   )
 }
-// // Import Swiper React
 
-
-// // Import modules de Swiper (v11+)
-
-
-// // Import estilos de Swiper
-
-
-// const CarouselProjects = () => {
-//   const projects = [
-//     { id: 1, title: 'Proyecto 1', img: '/images/project1.jpg', link: '#' },
-//     { id: 2, title: 'Proyecto 2', img: '/images/project2.jpg', link: '#' },
-//     { id: 3, title: 'Proyecto 3', img: '/images/project3.jpg', link: '#' },
-//   ];
-
-//   return (
-//     <div className="w-full my-10">
-//       <Swiper
-//         modules={[Autoplay, Navigation, Pagination]}
-//         spaceBetween={30}
-//         slidesPerView={1}
-//         navigation
-//         pagination={{ clickable: true }}
-//         autoplay={{ delay: 3000 }}
-//         loop
-//       >
-//         {projects.map((project) => (
-//           <SwiperSlide key={project.id}>
-//             <a href={project.link} target="_blank" rel="noopener noreferrer">
-//               <img src={project.img} alt={project.title} className="w-full h-auto rounded-lg shadow-lg" />
-//             </a>
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
-//     </div>
-//   );
-// };
-
-// export default CarouselProjects;
 
